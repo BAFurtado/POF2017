6ee795ae2360a8d7b9a7f777cc08dec5864959ba
furtadobb <furtadobb@gmail.com>
2019-10-05 19:38:23 -0300

    Separating despesas

--- unpacking_reading.py
+++ unpacking_reading.py
@@ -19,17 +19,21 @@ def retrieve(path):
 #     return str(state_id.iloc[0])
 
 
+def read_despesas(path, file, cols):
+    return pd.read_excel(os.path.join(path, file), header=7, names=cols, encoding='latin-1',
+                         skiprows=[61, 62, 63, 64, 65, 66, 67, 68, 69, 70], skipfooter=7, na_values='-')
+
+
 def read_ufs(path):
     output = dict()
     files = os.listdir(path)
     cols = ['name', 'total', 'classe 1', 'classe 2', 'classev3', 'classe 4', 'classe 5', 'classe 6', 'classe 7']
     pattern = r'\d\d'
     ufs = pd.read_csv('ufs_ids.csv', sep=';')
-    for i in files:
-        if re.match(pattern, i):
-            uf = ufs.loc[ufs['nummun'] == int(i[:2]), 'codmun'].iloc[0]
-            output[uf] = pd.read_excel(os.path.join(path, i), header=7, names=cols, encoding='latin-1',
-                              skiprows=[61, 62, 63, 64, 65, 66, 67, 68, 69, 70], skipfooter=7, na_values='-')
+    for file in files:
+        if re.match(pattern, file):
+            uf = ufs.loc[ufs['nummun'] == int(file[:2]), 'codmun'].iloc[0]
+            output[uf] = read_despesas(path, file, cols)
     return output
 
 
@@ -37,5 +41,5 @@ if __name__ == '__main__':
     p = r'originais/'
     # retrieve(p)
     o = read_ufs(p)
-    # for key in o.keys():
-    #     print(o[key].head(2))
+    for key in o.keys():
+        print(o[key].head(2))
